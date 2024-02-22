574315fcb22e2e8c9a144b409607ea78661578c9
LooVik <loovikwong@gmail.com>
2024-02-22 17:27:41 +0800

    Added Water Projectile ans Scripts
    Added projectiles for bullet and scripts which handle the shotting.

---
+++ WarOfTank/Assets/GameAssets/WaterBall/Scripts/WaterBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBall : MonoBehaviour {
+
+	private void Start() {
+		Destroy(gameObject, 4.0f);
+	}
+
+	private void OnTriggerEnter(Collider other) {
+//		if (other.CompareTag("Enemy")) {
+//			Destroy(gameObject);
+//		}
+	}
+}
---
+++ WarOfTank/Assets/Scripts/Cannons/ScreenCannons.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.Assertions;
+using UnityEngine;
+
+public class ScreenCannons : MonoBehaviour {
+
+	[SerializeField] private GameObject projectile;
+	[SerializeField] private int maxAmmo = 3;
+	[SerializeField] private float reloadTime = 5.0f;
+	[SerializeField] private float Forceshot = 750.0f;
+
+	private int curretnAmmo = 0;
+	private ScreenCannons instance;
+
+	public int MaxAmmo { get { return maxAmmo; }}
+	public int CurrentAmmo { get { return curretnAmmo; }}
+	public ScreenCannons Instance { get { return instance; }}
+
+	private void Awake()
+	{
+		//Making sure that throwing/shooting smtg.
+		Assert.IsNotNull(projectile);
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(ReloadAmmo());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			ShotFire();
+		}
+	}
+
+	private IEnumerator ReloadAmmo()
+	{
+		while (true)
+		{
+			curretnAmmo = Mathf.Min(curretnAmmo + 1, maxAmmo);
+			yield return new WaitForSeconds(reloadTime);
+		}
+	}
+
+	private void ShotFire()
+	{
+		if(curretnAmmo > 0 /*&& !EventSystem.current.IsPointerOverGameObject()*/)
+		{
+			GameObject newProjectile = Instantiate(projectile, transform.position, transform.rotation);
+			Rigidbody projBody = newProjectile.GetComponent<Rigidbody>();
+			projBody.AddForce(transform.forward * Forceshot);
+			curretnAmmo--;
+		}
+
+	}
+
+}
